e78b3714f46517ec03e9ba7b4a2c8e3fc8399dc4
Tomasz Puscian <pusciant@gmail.com>
2020-03-06 20:49:36 +0100

    upgrade

--- development/js/app.js
+++ development/js/app.js
@@ -1,11 +1,23 @@
 const input = document.querySelector("#userName");
 const button = document.querySelector("#setUserName");
 const userNameField = document.querySelector("#showName");
+const removeUser = document.querySelector("#removeUserName");
 
+userNameField.innerText = localStorage.getItem("name");
 
-button.addEventListener("click", function (e) {
+button.addEventListener("click", function () {
+    if (localStorage.getItem("name") === null) {
+        console.log("elo");
+        localStorage.setItem("name", input.value);
+        userNameField.innerText = localStorage.getItem("name")
+    } else {
+        userNameField.innerText = localStorage.getItem("name")
+    }
+});
+
+removeUser.addEventListener("click", function (e) {
     e.preventDefault();
-    localStorage.setItem("name", input.value);
-    userNameField.innerText = localStorage.getItem("name")
+    localStorage.removeItem("name");
+    userNameField.innerText = "";
 });
 
