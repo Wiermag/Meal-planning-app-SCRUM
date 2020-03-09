3a821a7924cd27173e3df66e36bea593dd234dbd
Tomasz Puscian <pusciant@gmail.com>
2020-03-09 19:44:21 +0100

    tp-setUserName-app

--- development/js/app.js
+++ development/js/app.js
@@ -1,23 +1,14 @@
 const input = document.querySelector("#userName");
-const button = document.querySelector("#setUserName");
+const button = document.querySelector("#submitName");
 const userNameField = document.querySelector("#showName");
-const removeUser = document.querySelector("#removeUserName");
 
 userNameField.innerText = localStorage.getItem("name");
 
 button.addEventListener("click", function () {
     if (localStorage.getItem("name") === null) {
-        console.log("elo");
         localStorage.setItem("name", input.value);
         userNameField.innerText = localStorage.getItem("name")
     } else {
         userNameField.innerText = localStorage.getItem("name")
     }
 });
-
-removeUser.addEventListener("click", function (e) {
-    e.preventDefault();
-    localStorage.removeItem("name");
-    userNameField.innerText = "";
-});
-
