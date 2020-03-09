fda5cdf66fa8a2ec1e291795b4cf4733f62fdce7
KarolKuczkowski <karolkuczkowski@gazeta.pl>
2020-03-09 16:23:37 +0100

    kkfooternew

--- development/js/index.js
+++ development/js/index.js
@@ -5,21 +5,22 @@ const next = document.querySelector(".fa-chevron-right");
 const carousel = document.querySelectorAll(".carousel-slide");
 
 let i = 0;
+
 function slide() {
-    if (i < 0 ){
-        i = carousel.length-1
+    if (i < 0) {
+        i = carousel.length - 1
     }
-    if (i >= carousel.length){
+    if (i >= carousel.length) {
         i = 0;
     }
-} 
-previous.addEventListener("click", function() {
+}
+previous.addEventListener("click", function () {
     carousel[i].classList.remove("visible");
     i--
     slide();
     carousel[i].classList.add("visible");
 });
-next.addEventListener("click", function() {
+next.addEventListener("click", function () {
     carousel[i].classList.remove("visible");
     i++
     slide()
