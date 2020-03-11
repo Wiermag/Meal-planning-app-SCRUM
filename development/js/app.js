94fe222ff53a4c02ca7e488e69a2900c37b13296
Tomasz Puscian <pusciant@gmail.com>
2020-03-11 20:39:45 +0100

    setname fix

--- development/js/app.js
+++ development/js/app.js
@@ -1,11 +1,18 @@
 const input = document.querySelector("#userName");
 const button = document.querySelector("#submitName");
 const userNameField = document.querySelector("#showName");
-const formContainer = document.querySelector(".container-userNameForm");
+const formContainer = document.querySelector(".userNameForm-container");
+
+// localStorage.removeItem("name");
 
 userNameField.innerText = localStorage.getItem("name");
 
-button.addEventListener("click", function () {
+if (userNameField.innerText !== "") {
+    userNameField.innerText = localStorage.getItem("name");
+    formContainer.classList.add("form-display")
+}
+
+button.addEventListener("click", function (e) {
     if (localStorage.getItem("name") === null) {
         localStorage.setItem("name", input.value);
         userNameField.innerText = localStorage.getItem("name")
@@ -14,5 +21,6 @@ button.addEventListener("click", function () {
         localStorage.setItem("name", input.value);
         userNameField.innerText = localStorage.getItem("name")
     }
+    e.preventDefault();
     formContainer.classList.add("form-display")
 });
